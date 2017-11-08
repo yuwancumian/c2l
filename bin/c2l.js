e841a79399bfe314b23f45033501a9e1a498819b
yuwancumian <yuwancumian@hotmail.com>
2017-11-08 17:16:11 +0800

    basic download feather

--- bin/c2l.js
+++ bin/c2l.js
@@ -5,7 +5,14 @@ const download = require("../lib/download")
 cli.command("*", "cdnjs to local", (input,flags)=>{
   console.log("worked")
   const url = input[0]
+  console.log(flags.output)
   // console.log(download)
   download(url)
 })
+
+cli.option("output",{
+  alias: "o",
+  desc: "output file derectory"
+})
 cli.parse()
+
