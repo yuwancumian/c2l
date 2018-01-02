88a9f722056aad022a4179b0bfcd361fed6aec27
yuwancumian <yuwancumian@hotmail.com>
2018-01-02 18:10:46 +0800

    with promise

--- bin/c2l.js
+++ bin/c2l.js
@@ -1,6 +1,7 @@
-const cac = require("cac")
-const cli = cac()
-const download = require("../lib/download")
+const cac = require("cac");
+const cli = cac();
+const ora = require("ora");
+const download = require("../lib/down");
 
 cli.command("*", "cdnjs to local", (input,flags)=>{
   console.log("worked")
@@ -8,11 +9,20 @@ cli.command("*", "cdnjs to local", (input,flags)=>{
   console.log(flags.output)
   // console.log(download)
   download(url)
-})
+});
 
 cli.option("output",{
   alias: "o",
   desc: "output file derectory"
-})
-cli.parse()
+});
+cli.parse();
 
+
+const spinner = ora();
+// sao.on('download:start', () => {
+//   spinner.text = 'Downloading...'
+//   spinner.start()
+// })
+// sao.on('download:stop', () => {
+//   spinner.stop()
+// })
--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 #!/usr/bin/env node
+
+var c2l = require("./bin/c2l")
+c2l()
